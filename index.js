361e0d8a3d833e728a90b554dedf43da596b50f0
ss592001 <90674743+ss592001@users.noreply.github.com>
2025-02-25 18:54:27 +0530

    Update index.js new

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const MongoDbStore = require('connect-mongodb-session')(Session);
 const mongoose = require('mongoose');
 const AuthRoutes = require('./Routes/Auth/Auth');
 const AdminRoutes=require('./Routes/Admin/Admin')
-
+const port = process.env.PORT || 4000;
 const mongoUrl = 'mongodb://sahil1234:sahil1234@ac-9k734zt-shard-00-00.hntpn8l.mongodb.net:27017,ac-9k734zt-shard-00-01.hntpn8l.mongodb.net:27017,ac-9k734zt-shard-00-02.hntpn8l.mongodb.net:27017/?ssl=true&replicaSet=atlas-gvtm46-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0';
 const Store = new MongoDbStore({
    uri: mongoUrl,
@@ -51,7 +51,7 @@ app.use('/QAUploads', express.static(path.join(__dirname, 'QAUploads')));
 app.use('/Images', express.static(path.join(__dirname, 'Images')));
 mongoose.connect(mongoUrl)
    .then(result => {
-      app.listen(10000);
+      app.listen(port);
       console.log("connected to db and terminal at 3008");
    })
 
